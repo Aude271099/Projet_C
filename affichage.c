bdfed85e245c2d1746bf6eccc4e115dff0136738
Banfra <50145407+Banfra@users.noreply.github.com>
2020-03-30 18:18:59 +0200

    update affichage

--- affichage.c
+++ affichage.c
@@ -4,8 +4,8 @@ void affichage(oxy myOxy){
     FILE *fp;
     FILE *verrou;
 
-    verrou = fopen(".verrouData", "w");//Création du verrou
-    fp = fopen("Data.txt", "w"); //Ouverture du fichier en écriture
+    verrou = fopen("./.verrouData", "w");//Création du verrou
+    fp = fopen("./Data.txt", "w"); //Ouverture du fichier en écriture
 
     if(fp == NULL){
         printf("Erreur lors de l'ouverture du fichier");
@@ -15,6 +15,7 @@ void affichage(oxy myOxy){
     fprintf(fp, "%d", myOxy.pouls); //Ecriture du pouls
 
     fclose(verrou); //On ferme le fichier verrou
-    remove(".verrouData"); //On supprime le verrou
+    remove("./.verrouData"); //On supprime le verrou
+    fclose(fp);
 }
 
